ae60c6db2093a805f7c6aee4875846278dd7818c
Nurzhan Bizhan <bknurik@gmail.com>
2022-12-07 18:05:54 +0600

    update

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let arr = [5, 2, 11, 15, 10];
-let count = 0;
-for (let i = 0; i < arr.length; i++) {
-  if (i < arr.length) {
-    if (arr[i] % 2 == 0) {
-      count++;
-    }
-  }
-}
-console.log(count);
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join('');
+});
